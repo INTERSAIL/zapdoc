d56385fddfe8fbc3fdf41e05e5c149d9bda28f4f
Jacopo Beschi <jbeschi@redmaxi.sail.local>
2014-11-26 10:22:55 +0100

    setted up activerecord and first migration

--- config/initializers/mongoid.rb
+++
@@ -1,3 +0,0 @@
-Mongoid.load!(Rails.root.join('config/mongoid.yml'))
-
-Mongoid.raise_not_found_error = false
---
+++ db/migrate/20141126092232_enable_uuid_ossp_extension.rb
@@ -0,0 +1,4 @@
+class EnableUuidOsspExtension < ActiveRecord::Migration
+  def change
+  end
+end
