aa7ea63a9d61f047623e8054a20de9930c7baf62
jacopo-beschi-intersail <jacopo.beschi@intersail.it>
2014-11-26 14:39:52 +0100

    added  down for enable uiid and hstore migrations

--- db/migrate/20141126092232_enable_uuid_ossp_extension.rb
+++ db/migrate/20141126092232_enable_uuid_ossp_extension.rb
@@ -1,5 +1,9 @@
 class EnableUuidOsspExtension < ActiveRecord::Migration
-  def change
+  def up
     enable_extension 'uuid-ossp'
+  end
+
+  def down
+    disable_extension 'uuid-ossp'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141126092232) do
+ActiveRecord::Schema.define(version: 20141126133421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
+  enable_extension "hstore"
 
 end
