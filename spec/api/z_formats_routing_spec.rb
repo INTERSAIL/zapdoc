c703ee6014e4fa19be8bf0b62d32003f0d4357ec
jacopo-beschi-intersail <jacopo.beschi@intersail.it>
2014-12-05 11:48:01 +0100

    fixed api specs

--- spec/api/z_formats_routing_spec.rb
+++ spec/api/z_formats_routing_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+require 'api_helper'
 
 RSpec.describe 'Formats', type: :request do
-  def expect_format(json, identifier, mime_type, default)
-    sel = json.select { |j| j[:identifier] == identifier.to_s }
+  def expect_format(json, id, mime_type, default)
+    sel = json.select { |j| j[:id] == id }
 
     expect(sel.count).to eq(1)
     expect(sel[0][:default]).to eq(default)
@@ -11,43 +11,46 @@ RSpec.describe 'Formats', type: :request do
 
   before(:all) do
     ZFormat.destroy_all
-    ZFormat.create!(description: 'Unknown format', mime_type: 'application/octet-stream', accepted_extensions: [], default: true)
-    ZFormat.create!(description: 'PDF', mime_type: 'application/pdf', accepted_extensions: ['pdf'], default: false)
-    ZFormat.create!(description: 'PKCS#7', mime_type: 'application/pkcs7-data', accepted_extensions: ['pdf'], default: false)
-    ZFormat.create!(description: 'Plain Text', mime_type: 'text/plain', accepted_extensions: ['txt'], default: false)
+    @unknown = ZFormat.create!(description: 'Unknown format', mime_type: 'application/octet-stream', accepted_extensions: [], default: true)
+    @pdf = ZFormat.create!(description: 'PDF', mime_type: 'application/pdf', accepted_extensions: ['pdf'], default: false)
+    @p7m = ZFormat.create!(description: 'PKCS#7', mime_type: 'application/pkcs7-data', accepted_extensions: ['pdf'], default: false)
+    @txt = ZFormat.create!(description: 'Plain Text', mime_type: 'text/plain', accepted_extensions: ['txt'], default: false)
   end
 
-  #@jtodoIMP fix this specs and setup then do the seeds
-
-  xit 'should list known formats' do
+  it 'should list known formats' do
     get '/api/z_formats'
-    expect(response.status).to eq(200)
 
+    expect(response).to be_success
     expect(json.count).to eq(4)
   end
 
-  xit 'list must include :unknown with mime_type application/octet-stream and default=true' do
+  it 'list must include :unknown with mime_type application/octet-stream and default=true' do
     get '/api/z_formats'
-    expect_format(json, :unknown, 'application/octet-stream', true)
+
+    expect_format(json, @unknown.id, 'application/octet-stream', true)
   end
 
-  xit 'list must include :pdf with mime_type application/pdf' do
+  it 'list must include :pdf with mime_type application/pdf' do
     get '/api/z_formats'
-    expect_format(json, :pdf, 'application/pdf', false)
+
+    expect_format(json, @pdf.id, 'application/pdf', false)
   end
 
-  xit 'list must include :p7m with mime_type application/pkcs7-data' do
+  it 'list must include :p7m with mime_type application/pkcs7-data' do
     get '/api/z_formats'
-    expect_format(json, :p7m, 'application/pkcs7-data', false)
+
+    expect_format(json, @p7m.id, 'application/pkcs7-data', false)
   end
 
-  xit 'list must include :txt with mime_type text/plain' do
+  it 'list must include :txt with mime_type text/plain' do
     get '/api/z_formats'
-    expect_format(json, :txt, 'text/plain', false)
+
+    expect_format(json, @txt.id, 'text/plain', false)
   end
 
-  xit 'should return the default format' do
+  it 'should return the default format' do
     get '/api/z_formats/default'
-    expect_format([json], :unknown, 'application/octet-stream', true)
+
+    expect_format([json], @unknown.id, 'application/octet-stream', true)
   end
 end
