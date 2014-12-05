53488f4c77c86830aab99e37996a087066d3cb73
jacopo-beschi-intersail <jacopo.beschi@intersail.it>
2014-12-05 18:02:09 +0100

    put valid_id protected

--- app/controllers/concerns/Postgres.rb
+++ app/controllers/concerns/Postgres.rb
@@ -3,20 +3,19 @@ module Concerns
     extend ActiveSupport::Concern
 
     included do
+      @valid_ids = []
       before_filter ->(id=params[:folder_id]) {validate_id id}
       before_filter ->(id=params[:id]) {validate_id id}
-
-      @valid_ids = []
     end
 
     module ClassMethods
       attr_accessor :valid_ids
 
+      protected
       def inherited(subclass)
         subclass.inherit_attributes(@valid_ids)
       end
 
-      protected
       def inherit_attributes(attrs)
         @valid_ids ||= []
         @valid_ids.concat attrs
